a64f08d5dfbf6c8de4ab83e7395db4a68036753f
Sajad Shafi <maliksajad004@gmail.com>
2026-07-16 22:36:48 +0530

    Changed theme to everest green

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -8,58 +8,20 @@ end
 
 return {
     {
-
-        "folke/tokyonight.nvim",
+        "neanias/everforest-nvim",
+        version = false,
         lazy = false,
         priority = 1000,
-        opts = {},
-
-        -- "sajadshafi/nebula.nvim",
         config = function()
-            vim.cmd.colorscheme("tokyonight")
+            require("everforest").setup({
+                background = "medium",       -- soft | medium | hard
+                transparent_background_level = 2,
+                italics = true,
+                disable_italic_comments = false,
+                ui_contrast = "low",         -- dim, easy on the eyes
+            })
+            vim.cmd.colorscheme("everforest")
             enable_transparency()
-
-            -- Example: enable transparency
-            -- require("nebula").setup({ transparent = true })
-            -- require("nebula").apply()
-
-            -- -- Lualine integration example:
-            -- require('lualine').setup({ options = { theme = require('nebula').lualine_theme() } })
         end
     }
-    --     {
-    --         "catppuccin/nvim",
-    --         name = "catppuccin",
-    --         config = function()
-    --             require("catppuccin").setup({
-    --                 flavour = "mocha", -- latte, frappe, macchiato, mocha
-    --                 transparent_background = true,
-    --                 integrations = {
-    --                     treesitter = true,
-    --                     native_lsp = {
-    --                         enabled = true,
-    --                         underlines = {
-    --                             errors = { "undercurl" },
-    --                             hints = { "undercurl" },
-    --                             warnings = { "undercurl" },
-    --                             information = { "undercurl" },
-    --                         },
-    --                     },
-    --                     cmp = true,
-    --                     gitsigns = true,
-    --                     telescope = {
-    --                         enabled = true
-    --                     },
-    --                     lualine = true,
-    --                     indent_blankline = { enabled = true },
-    --                     mason = true,
-    --                     notify = true,
-    --                 },
-    --             })
-    --
-    --             -- Load colorscheme
-    --             vim.cmd.colorscheme("catppuccin")
-    --             enable_transparency()
-    --         end,
-    --     }
 }
